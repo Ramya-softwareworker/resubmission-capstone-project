3ca0210931c075d9a6a33ecb4d5722eaee70d4b5
Ramya-softwareworker <ramya04032001@gmail.com>
2024-09-14 13:02:57 +0530

    Add files via upload

---
+++ Revaluvation/Expense-Tracker-App/frontend/src/requestMethods.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = "https://expense-tracker-pi-mauve.vercel.app";
+
+const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default publicRequest;
